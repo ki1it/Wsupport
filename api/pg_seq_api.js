595a9231d793773340fca6ad5f4ba9581b5c14f4
deadlorpa <deadlorpa@gmail.com>
2018-07-24 15:57:29 +0700

    77

--- api/pg_seq_api.js
+++ api/pg_seq_api.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize')
 
-const List_projects = sequelize.import('../bd_models/list_projects')
-const List_sup_workers = sequelize.import('../bd_models/list_sup_workers')
-const Messages = sequelize.import('../bd_models/messages')
-const Messages_group = sequelize.import('../bd_models/messages_group')
+const List_projects = sequelize.import('../db_seq/list_projects')
+const List_sup_workers = sequelize.import('../db_seq/list_sup_workers')
+const Messages = sequelize.import('../db_seq/messages')
+const Messages_group = sequelize.import('../db_seq/messages_group')
 List_projects.sync()
 List_projects.findAll().then(users => {
   console.log(users)
