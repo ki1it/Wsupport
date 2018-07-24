2dfb6908991728abe5dd79f3a50a96f0a07badf0
deadlorpa <deadlorpa@gmail.com>
2018-07-24 17:09:47 +0700

    deadlorpa made it

--- db_seq/sql_seq_api.js
+++ db_seq/sql_seq_api.js
@@ -9,11 +9,14 @@
 // //console.log(result);
 //     return result;
 // }
-
+const list_projects = require('./list_projects')
+const messages_group = require('./messages_group')
+const messages = require('./messages')
+const list_sup_workers = require('./list_sup_workers')
 const db = require('./db_init')
-async function GetManagers() {
-    db()
-return 0
+async function getAllManagers() {
+    let res = await list_sup_workers.findAll()
+    return res
 }
 async  function getAllProjects(){
     let res = await  list_projects.findAll()
