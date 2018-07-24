1bf53c27eda2234fff1bc35fcd991d153d7d544e
Kirill <kirsh.ru@yandex.ru>
2018-07-24 16:59:38 +0700

    added some files

---
+++ db_seq/sql_seq_api.js
@@ -0,0 +1,12 @@
+// async function GetProjects() {
+//     var result = await pgapi.pool.query('select * from new_schema.list_projects');
+// //console.log(result);
+//     return result;
+// }
+// async function GetManagers() {
+//     var result = await pgapi.pool.query('select count(DISTINCT new_schema.messages_group.chat_id),new_schema.messages_group.to_id, ' +
+//         'new_schema.list_sup_workers.name from new_schema.messages_group inner join  new_schema.list_sup_workers on(tel_number = to_id) group by to_id, name');
+// //console.log(result);
+//     return result;
+// }
+
