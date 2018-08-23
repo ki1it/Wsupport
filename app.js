e6b30c07caf8e600a68fb75f5a5963f589dd5f13
Kirill <kirsh.ru@yandex.ru>
2018-08-23 16:25:44 +0700

    index route fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 apitg.call()
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/statistics', statisticsRoute);
 app.use('/manager', managerRoute);
 app.use('/projectstatistics', projstatRoute);
